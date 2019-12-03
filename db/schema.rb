078ec522825dc2917ef9ed72075bc40b6dae24b9
R David N H <rdavidnh@gmail.com>
2019-12-03 15:26:38 +0300

    added columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 2019_12_03_114008) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "role"
+    t.string "phone"
+    t.string "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
