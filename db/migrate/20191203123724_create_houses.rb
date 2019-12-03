3812f6ec53d88bfeb8f3b794cf54579b075d8381
BeAnjara <beanjarahoussen@gmail.com>
2019-12-03 15:42:41 +0300

    Add tables houses, types, features and set up foreign key

---
+++ db/migrate/20191203123724_create_houses.rb
@@ -0,0 +1,14 @@
+class CreateHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :houses do |t|
+      t.string :title
+      t.text :description
+      t.integer :room_number
+      t.string :address
+      t.references :city, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :type, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_121359) do
+ActiveRecord::Schema.define(version: 2019_12_03_123724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,36 @@ ActiveRecord::Schema.define(version: 2019_12_03_121359) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "role"
-    t.string "phone"
+  create_table "features", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "room_number"
     t.string "address"
+    t.bigint "city_id"
+    t.bigint "user_id"
+    t.bigint "type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_houses_on_city_id"
+    t.index ["type_id"], name: "index_houses_on_type_id"
+    t.index ["user_id"], name: "index_houses_on_user_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -41,4 +65,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_121359) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "houses", "cities"
+  add_foreign_key "houses", "types"
+  add_foreign_key "houses", "users"
 end
