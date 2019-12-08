c45d9fa7c6157b5fb94a074ddb2ec92aa43af681
Rabedaoro David N H <rdavidnh@gmail.com>
2019-12-08 21:50:54 +0300

    added files + updated UI + added new styles + added validations

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,15 @@
 class HousesController < ApplicationController
 
+    layout 'simple_layout', only: [:edit, :new]
+
     def index
         @houses = House.all
+        render layout: 'secondary_layout'
     end
 
     def show
         @house = House.find(params[:id])
+        render layout: 'secondary_layout'
     end
 
     def new
@@ -13,52 +17,60 @@ class HousesController < ApplicationController
     end
 
     def create
-        puts params
+        # puts params
+        # return
 
         @house = House.new(
-            title: params[:title],
-            description: params[:description],
-            room_number: params[:room_number],
-            address: params[:address],
-            price: params[:price],
+            title: params[:house][:title],
+            description: params[:house][:description],
+            room_number: params[:house][:room_number],
+            address: params[:house][:address],
+            price: params[:house][:price],
             status: params[:house][:status],
-            is_available: params[:is_available],
-            district: District.find(params[:house][:district_id]),
+            is_available: true,
             user: User.find(1),
-            type: Type.find(params[:house][:type_id]),
+            district: District.find_by(id: params[:house][:district_id]),
+            type: Type.find_by(id: params[:house][:type_id])
         )
+
+        @house.feature_ids=(params[:house][:features]);
 
         if @house.save
             redirect_to house_path(@house.id), flash: {:success => 'Élément créé avec succès'}
+        else
+            render :new, layout: 'simple_layout'
         end
 
     end
 
     def edit
         @house = House.find(params[:id])
-
     end
 
     def update
-        p params
-
+        # p params
+        # return
         @house = House.find(params[:id])
 
         res = @house.update(
-            title: params[:title],
-            description: params[:description],
-            room_number: params[:room_number],
-            address: params[:address],
-            price: params[:price],
+            title: params[:house][:title],
+            description: params[:house][:description],
+            room_number: params[:house][:room_number],
+            address: params[:house][:address],
+            price: params[:house][:price],
             status: params[:house][:status],
-            is_available: params[:is_available],
-            district: District.find(params[:house][:district_id]),
+            is_available: params[:house][:is_available],
             user: User.find(1),
-            type: Type.find(params[:house][:type_id]),
+            district: District.find_by(id: params[:house][:district_id_id]),
+            type: Type.find_by(id: params[:house][:type_id_id])
         )
+
+        @house.feature_ids=(params[:house][:features]);
 
         if res
             redirect_to house_path(@house.id), flash: {:success => 'Élément modifié avec succès'}
+        else
+            render :edit, layout: 'simple_layout'
         end
 
     end
