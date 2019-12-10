7adcb71088858ac523e33261b38c3079c3f5c156
Rabedaoro David N H <rdavidnh@gmail.com>
2019-12-10 15:24:30 +0300

    updated Like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,15 +4,22 @@ class LikesController < ApplicationController
         p params
 
         house = House.find(params[:house_id])
-        like = house.likes.new(user: User.find(1))
+        like = house.likes.find_by(user: 1)
 
-        if like.save
+        if like
+            like.destroy()
             redirect_to houses_path
+        else
+            like = house.likes.new(user: User.find(1))
+
+            if like.save
+                redirect_to houses_path
+            end
         end
 
     end
 
-    def destroy
-        p params
-    end
+    # def destroy
+    #     p params
+    # end
 end
