1ef0c53305f8e1f6a65440fdfb2fe5bee73c73c3
BeAnjara <beanjarahoussen@gmail.com>
2019-12-13 19:29:33 +0300

    Fix bug in the signin modal

--- app/assets/javascripts/signin_modal.js
+++ app/assets/javascripts/signin_modal.js
@@ -1,6 +1,8 @@
 $(function(){
-    document.querySelector('.img__btn').addEventListener('click', function() {
-    document.querySelector('.cont').classList.toggle('s--signup');
+	$('#log-in').click(function(){
+	    document.querySelector('.img__btn').addEventListener('click', function() {
+	    document.querySelector('.cont').classList.toggle('s--signup');
+	});
   });
 });
 
