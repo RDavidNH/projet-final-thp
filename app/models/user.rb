af7cd9c62aa1ba951ad0e9f94201cd4130e8afeb
BeAnjara <beanjarahoussen@gmail.com>
2019-12-13 09:12:13 +0300

    Set user to has one avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
   belongs_to :city, optional: :true
   has_one :testimonial
   has_many :houses
