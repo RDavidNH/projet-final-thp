4d78b454c69b675fd6b0b59635122619547968d7
BeAnjara <beanjarahoussen@gmail.com>
2019-12-19 14:47:04 +0300

    Build routes to accede to the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/hoomiesadmin', as: 'rails_admin'
-  mount RailsAdmin::Engine => '/superadmin', as: 'clientadmin'
+  # mount RailsAdmin::Engine => '/superadmin', as: 'clientadmin'
   get 'users/profil', 'users#profil'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
