3c579595d7f92fa9583ce3b9e7e87711c3dc349e
BeAnjara <beanjarahoussen@gmail.com>
2020-01-09 14:30:00 +0300

    Change the redirection path if user signin wit social media is fail

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,14 +4,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     puts request.env["omniauth.auth"]
     puts request.env["omniauth.auth"].info.email
     puts "-" * 100
-    puts @user
+   
     puts "*" * 40
-    @user = UserProvider.find_for_facebook_oauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication               
+    if request.env["omniauth.auth"].info.email
+      puts "a"* 200
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      @user = UserProvider.find_for_facebook_oauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication               
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
   end
 
