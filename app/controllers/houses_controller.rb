5dfd9351d8a4fc856f9759220d6d79963fe6f24e
fanomallot <aafano19@gmail.com>
2020-01-09 15:11:13 +0300

    updated house edit upload

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -11,11 +11,11 @@ class HousesController < ApplicationController
     def show
         @house = House.find(params[:id])
 
-        puts "*"*33 
-        for h in @house.photos 
-            p url_for h
-        end
-        puts "*"*33
+        # puts "*"*33 
+        # for h in @house.photos 
+        #     p url_for h
+        # end
+        # puts "*"*33
 
         render layout: 'secondary_layout'
     end
@@ -60,6 +60,7 @@ class HousesController < ApplicationController
     def update
         # puts "*"*33
         # p params
+        # # p params.key?(:delete_photos)
         # puts "*"*33
         
         # return
@@ -69,8 +70,11 @@ class HousesController < ApplicationController
         # puts "*"*33
         # p @house.photos.find([5, 6])
         # puts "*"*33
-        @house.photos.find([5, 6]).purge
-return 
+        # if params.has
+            
+        # end
+# return 
+
         res = @house.update(
             title: params[:house][:title],
             description: params[:house][:description],
@@ -83,11 +87,33 @@ return
             district: District.find_by(id: params[:house][:district_id]),
             type: Type.find_by(id: params[:house][:type_id])
         )
-
-        for picture in params[:photos]
-            @house.photos.attach(picture)
+        
+        if params.key?(:photos)
+            for picture in params[:photos]
+                @house.photos.attach(picture)
+            end
+            # @house.photos.attach(params[:photos])
         end
-        # @house.photos.attach(params[:photos])
+
+        if params.key?(:edit_photos)
+            params[:edit_photos].each do |k, v|
+                res = @house.photos.attach(v)
+                # puts "*"*33
+                # puts "lol " * 33
+                # p res.first[:id]
+                # puts "*"*33
+                if res.length > 0
+                    @house.photos.find(k).purge
+                end
+            end
+            # @house.photos.attach(params[:photos])
+        end
+
+        if params.key?(:delete_photos)
+            for photo in @house.photos.find(params[:delete_photos])
+                photo.purge
+            end
+        end
 
         @house.feature_ids=(params[:house][:features]);
 
