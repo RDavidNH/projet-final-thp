5dfef557db3f97fdd73ce0fd4b3001e72ac81692
BeAnjara <beanjarahoussen@gmail.com>
2020-01-20 15:23:26 +0300

    Updated with method to control user owner house account

--- app/models/house.rb
+++ app/models/house.rb
@@ -19,6 +19,7 @@ class House < ApplicationRecord
     validates :district_id, presence: true
     validates :type_id, presence: true
     validates :is_available, presence: true
+    validate :check_user_post_status, on: :create
 
     rails_admin do
         configure :comments do
@@ -46,6 +47,12 @@ class House < ApplicationRecord
         end
 
 
+    end
+
+    def check_user_post_status
+        if self.user.can_post == false
+            errors.add(:message, 'Upgrade your count to Gold to have possibility to post 3 to 6 articles')
+        end
     end
 
 end
