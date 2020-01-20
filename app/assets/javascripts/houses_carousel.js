ed8625a2b4a9bbad89dd6acc0478b63aa9d4dbfe
RDavidNH <rdavidnh@gmail.com>
2020-01-20 10:33:41 +0300

    fixed compilation error houses_carousel js

--- app/assets/javascripts/houses_carousel.js
+++ app/assets/javascripts/houses_carousel.js
@@ -4,11 +4,11 @@ $(function(){
         var highlight = document.querySelector(".gallery-hightlight");
         var previews = document.querySelectorAll(".room-preview img");
         
-        previews.forEach(preview => {
+        previews.forEach(function (preview) {
             preview.addEventListener("click", function() {
                 const smallSrc = this.src;
                 // const bigSrc = smallSrc.replace("small", "big");
-                previews.forEach(preview => preview.classList.remove("room-active"));
+                previews.forEach(function (preview) { preview.classList.remove("room-active") });
                 highlight.src = smallSrc;
                 preview.classList.add("room-active");
             });
