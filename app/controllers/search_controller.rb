a97c5aad166dc3be82511a238f7b9da19cefbb5e
Rabedaoro David N H <rdavidnh@gmail.com>
2020-01-28 15:25:46 +0300

    added filter by price range feature to houses

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,6 +11,24 @@ class SearchController < ApplicationController
             criterion[:status] = params[:status]
         end
 
+        price_rent_ids = {
+            1 => 100000..250000,
+            2 => 250000..500000,
+            3 => 500000..100000,
+        }
+
+        price_sell_ids = {
+            1 => 10000000..15000000,
+            2 => 15000000..20000000,
+            3 => 20000000..30000000,
+        }
+
+        if params[:status] == 'rent'
+            criterion[:price] = price_rent_ids[params[:rent_price].to_i]
+        else params[:status] == 'sell'
+            criterion[:price] = price_sell_ids[params[:sell_price].to_i]
+        end
+
         @houses = House.where(criterion)
 
         render layout: 'secondary_layout'
