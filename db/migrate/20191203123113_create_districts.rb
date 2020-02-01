51773c679a2cfc6da482e54dec5341f58d064cac
BeAnjara <beanjarahoussen@gmail.com>
2020-02-01 19:23:19 +0300

    Add column latitude and longitude for the mapping

--- db/migrate/20191203123113_create_districts.rb
+++ db/migrate/20191203123113_create_districts.rb
@@ -2,6 +2,8 @@ class CreateDistricts < ActiveRecord::Migration[5.2]
   def change
     create_table :districts do |t|
       t.string :name
+      t.float :longitude
+      t.float :latitude
       t.text :description
       t.references :city, foreign_key: true
 
