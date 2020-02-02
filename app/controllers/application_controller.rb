1fb5f12d031efb862bc73a501f41e24601e23fbb
BeAnjara <beanjarahoussen@gmail.com>
2020-02-02 20:50:04 +0300

    Remove flash login failed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
 
      def authenticate_user
        unless current_user
-         flash[:danger] = "Please log in."
+        #  flash[:danger] = "Please log in."
          redirect_to new_user_session_path
        end
      end
