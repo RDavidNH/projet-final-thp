ff92085c76539a797997711f9eb3c2d9650c404e
BeAnjara <beanjarahoussen@gmail.com>
2020-02-03 12:20:11 +0300

    Edit districts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,128 @@ features_obj = []
 types = ['Villa', 'Appartement', 'Bureau', 'Studio', 'Manoir']
 status = ['sell', 'rent']
 features = ['wc', 'douche', 'garage']
+districts_hash = {
+    "Analakely" => {
+        latitude: -18.908281,
+        longitude: 47.52692
+    },
+    "Itaosy" => {
+        latitude: -18.908281,
+        longitude: 47.470842
+    },
+    "Ambohijatovo" => {
+        latitude: -18.702808,
+        longitude: 47.363262
+    },
+    "Ankorondrano" => {
+        latitude: -18.886758,
+        longitude: 47.521299
+    },
+    "Ivandry" => {
+        latitude: -18.87565,
+        longitude: 47.525107
+    },
+    "Ankadikely" => {
+        latitude: -18.848572,
+        longitude: 47.553342
+    },
+    "Ankatso" => {
+        latitude: -18.915987,
+        longitude: 47.565771
+    },
+    "Ambanidia" => {
+        latitude: -18.918807,
+        longitude: 47.538123
+    },
+    "Analamahitsy" => {
+        latitude: -18.876794,
+        longitude: 47.546878
+    },
+    "Andraharo" => {
+        latitude: -18.884608,
+        longitude: 47.509932
+    },
+    "Alarobia" => {
+        latitude: -18.228891,
+        longitude: 47.753851
+    },
+    "Ambodivona" => {
+        latitude: -18.683295,
+        longitude: 47.576698
+    },
+    "Antanimena" => {
+        latitude: -18.898609,
+        longitude: 47.520054
+    },
+    "Ankadifotsy" => {
+        latitude: -18.717549,
+        longitude: 47.550704
+    },
+    "Ankazomanga" => {
+        latitude: -18.89162,
+        longitude: 47.513574
+    },
+    "Anosy" => {
+        latitude: -18.916398,
+        longitude: 47.518638
+    },
+    "Faravohitra" => {
+        latitude: -18.811799,
+        longitude: 47.56663
+    },
+    "Antaninarenina" => {
+        latitude: -18.910296,
+        longitude: 47.525018
+    },
+    "Ampasampito" => {
+        latitude: -18.896343,
+        longitude: 47.546264
+    },
+    "Anjanahary" => {
+        latitude: -18.895872,
+        longitude: 47.538433
+    },
+    "Ankadivato" => {
+        latitude: -18.910493,
+        longitude: 47.533607
+    },
+    "Tsimbazaza" => {
+        latitude: -18.932596,
+        longitude: 47.52749
+    },
+    "Ambatobe" => {
+        latitude: -18.879389,
+        longitude: 47.558595
+    },
+    "Mahamasina" => {
+        latitude: -18.919114,
+        longitude: 47.524365
+    },
+    "Ambohibao" => {
+        latitude: -18.916398,
+        longitude: 47.518638
+    },
+    "Amboditsiry" => {
+        latitude: -18.885443,
+        longitude: 47.538765
+    },
+    "Ambatomainty" => {
+        latitude: -18.55296,
+        longitude: 47.508332
+    },
+    "Ambohimanarina" => {
+        latitude: -18.360198,
+        longitude: 47.135528
+    },
+    "Ankerana" => {
+        latitude: -18.888534,
+        longitude: 47.55577
+    },
+    "Nanisana" => {
+        latitude: -18.891785,
+        longitude: 47.549472
+    }
+    }
 
 10.times do
     cities << City.create(
@@ -28,14 +150,23 @@ city_admin = City.create(
         zip_code: 101
     )
 
-20.times do
+# 20.times do
+#     districts << District.create(
+#         name: Faker::Address.street_name,
+#         description: Faker::Lorem.paragraph(sentence_count: 5),
+#         city: cities[rand(0..cities.size)]
+#     )
+# end
+
+districts_hash.each do |district, coord|
     districts << District.create(
-        name: Faker::Address.street_name,
+        name: district,
+        latitude: coord[:latitude],
+        longitude: coord[:longitude],
         description: Faker::Lorem.paragraph(sentence_count: 5),
         city: cities[rand(0..cities.size)]
     )
 end
-
 users << User.create(
         email: 'hoomiesadmin@yopmail.com',
         password: 'hoomiesadmin',
